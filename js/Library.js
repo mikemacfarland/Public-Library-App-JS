82d20aa23073b8ead2075e87d6fa2c5b280a1f4b
Mike Macfarland <m_macfarland@live.com>
2021-06-16 14:01:18 -0600

    added aditional project files, modified HTML

--- js/Library.js
+++ js/Library.js
@@ -18,7 +18,7 @@ class Library{
     //  charge fines is ran again.
 
     // also fines get charged positive when book isnt overdue
-    
+
     chargeFines(){
         if (this.patrons.currentbook !== []){
         let patrons = library.patrons
@@ -40,3 +40,35 @@ class Library{
     
     }
 }
+
+// from html
+
+<script defer>
+  // our test code here
+  const library = new Library()
+  
+
+  const book1 = new Book("Ass Goblins of Auschwitz","Cameron Pierce","1933929936","08-05-09")
+  const book2 = new Book("The Baby Jesus Butt Plug","Carleton Mellic III","0972959823","01-23-04")
+  const book3 = new Book("I Knocked Up Satan's Daughter","Carleton Mellic III","1936383829","09-06-11")
+
+  const patron1 = new Patron("Dale","440rupp69@hotmail.com")
+  const patron2 = new Patron("Jeff","mrpenis@hotmail.com")
+  
+  library.addPatron(patron1)
+  library.addPatron(patron2)
+
+  library.addBook(book1)
+  library.addBook(book2)
+  library.addBook(book3)
+
+  patron1.checkout(book1)
+  patron1.checkout(book2)
+  patron2.checkout(book2)
+  library.chargeFines()
+  // console.log(patron1)
+
+  
+  
+  
+</script>
